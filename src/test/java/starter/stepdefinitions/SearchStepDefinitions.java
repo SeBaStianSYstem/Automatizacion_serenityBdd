e011b05b8c9303729354db2233ffb7e1f0e5c0ec
1TaTAn5 <sebastatan254@gmail.com>
2024-12-01 13:54:50 -0500

    desarrollo stepdefinitions

--- src/test/java/starter/stepdefinitions/SearchStepDefinitions.java
+++ src/test/java/starter/stepdefinitions/SearchStepDefinitions.java
@@ -9,24 +9,33 @@ import net.serenitybdd.screenplay.questions.page.TheWebPage;
 import starter.navigation.NavigateTo;
 import starter.search.LookForInformation;
 
+import java.util.Objects;
+
+
 public class SearchStepDefinitions {
+    private static final String paginaFacebook = "pages.facebook";
 
-    @Given("{actor} is researching things on the internet")
-    public void researchingThings(Actor actor) {
-        actor.wasAbleTo(NavigateTo.theSearchHomePage());
-    }
-
-    @When("{actor} looks up {string}")
-    public void searchesFor(Actor actor, String term) {
-        actor.attemptsTo(
-                LookForInformation.about(term)
+    @Given("que el usuario se encuentre en la pagina inicial de facebook")
+    public void queElUsuarioSeEncuentreEnLaPaginaInicialDeFacebook() {
+        COMMON_ACTOR.attemptsTo(
+                Open.browserOn().thePageNamed(paginaFacebook)
         );
     }
 
-    @Then("{actor} should see information about {string}")
-    public void should_see_information_about(Actor actor, String term) {
-        actor.attemptsTo(
-                Ensure.that(TheWebPage.title()).containsIgnoringCase(term)
-        );
+    @When("da click en el boton log in utilizando el metodo {string}")
+    public void clickBotonLogin(String metodo){
+        if(Objects.equals(metodo, "ContainingText")){
+            COMMON_ACTOR.attemptsTo(clickLoginButtonFacebookContainingText());
+        }
+        else if (Objects.equals(metodo,"WithNameOrId")) {
+            COMMON_ACTOR.attemptsTo(clickLoginButtonFacebookWithNameOrId());
+        }else {
+            System.out.println("Metodo no disponible");
+        }
+    }
+
+    @Then("visualizara la pagina de facebook con el texto {string}")
+    public void assertionClickBotonLoginFacebook(String text){
+        COMMON_ACTOR.should(seeThat(CommonQuestions.textEquals(text, textoEsperadoFacebookLogin)));
     }
 }
